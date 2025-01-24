6f8515221b7efe33043a22ac06210e2511cefe87
TeenCoder159 <no1umang@gmail.com>
2025-01-24 22:11:30 +0800

    Add support for more languages

--- src/writer.rs
+++ src/writer.rs
@@ -1,29 +1,27 @@
 use std::fs;
 
+fn file_creator(file_name: &String, file_content: &str) {
+    fs::write(file_name, file_content).expect("An Unexpeceted error occured, please try again!");
+}
+
 pub fn write(file_ext: &str, file_name: &String) {
     match file_ext {
-        "py" => {
-            fs::write(file_name, r#"print("Hello Lion!")"#).expect("Unable to create file");
-        }
-        "rs" => {
-            fs::write(
-                file_name,
-                r#"fn main() {
-    println!("Hello Lion!");
-}"#,
-            )
-            .expect("Unable to create file");
-        }
-        "cpp" => fs::write(
-            file_name,
-            r#"#include <iostream>
+        "py" => file_creator(file_name, r#"print("Hello Lion!")"#),
 
-int main() {
-    std::cout << "Hello, Lion!" << std::endl;
-    return 0;
-}"#,
-        )
-        .expect("Unable to create file"),
+        "rs" => file_creator(file_name, "fn main() {\n    println!(\"Hello Lion!\");\n}"),
+        "cpp" => file_creator(
+            file_name,
+            "#include <iostream>\n\nint main() {\n    std::cout << \"Hello, Lion!\" << std::endl;\n    return 0;\n}",
+        ),
+        "c" => file_creator(file_name, "#include <stdio.h>
+
+        int main() {
+            printf(\"Hello Lion!\");
+            return 0;
+        }"),
+        "go" => file_creator(
+            file_name,
+            "package main\n\nimport \"fmt\"\n\nfunc main() {\n    fmt.Println(\"Hello Lion!\")\n}"),
         _ => panic!("Unsupported file format or an error occured!"),
     }
 }
