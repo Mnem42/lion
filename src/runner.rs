fe7788b23362762a8da8512ece1dce07be9333a9
TeenCoder159 <no1umang@gmail.com>
2025-01-26 03:00:03 +0800

    First commit

---
+++ src/runner.rs
@@ -0,0 +1,13 @@
+//use std::process::Command;
+
+pub fn _run(file_ext: &str, _file_name: &String) {
+    match file_ext {
+        "cpp" => {
+            //let Command::new("g++").Command::arg("{file_name}").arg("-o").arg("output").output();
+            todo!()
+        }
+        _ => {
+            todo!()
+        }
+    }
+}
