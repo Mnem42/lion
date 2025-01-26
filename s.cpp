48e4425f31685baa36ae8ca758e2a2c320b51302
TeenCoder159 <no1umang@gmail.com>
2025-01-26 13:53:34 +0530

    Add running capabilities

--- s.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-
-int main() {
-    std::cout << "Hello, Lion!" << std::endl;
-    return 0;
-}
--- src/runner.rs
+++ src/runner.rs
@@ -3,30 +3,39 @@ use std::process::Command;
 pub fn run(file_ext: &str, file_name: &String) {
     match file_ext {
         "cpp" => {
-            let _ = Command::new("g++")
-                .arg("{file_name}")
-                .arg("-o")
-                .arg("output")
-                .output()
-                .expect("An error occured; Please try again.");
-            println!("Running you")
+            let output = {
+                Command::new("g++")
+                    .arg(file_name)
+                    .arg("-o")
+                    .arg(file_name)
+                    .spawn()
+                    .expect("An error occured; Please try again.");
+                Command::new(format!("./{file_name}"))
+                    .spawn()
+                    .expect("An error occured; Please try again.")
+            };
+            println!("Running");
+            output.stdout;
         }
         "rs" => {
-            let _ = Command::new("rustc")
-                .arg(file_name)
-                .output()
-                .expect("An error occured; Please try again.");
-            if cfg!(target_os = "windows") {
+            let output = if cfg!(target_os = "windows") {
                 Command::new("rustc")
-                    .arg(format!("{file_name}.exe"))
+                    .arg(file_name)
+                    .output()
+                    .expect("An error occured; Please try again.");
+                Command::new(format!(".\\{file_name}.exe"))
                     .output()
                     .expect("An error occured; Please try again.")
             } else {
                 Command::new("rustc")
                     .arg(file_name)
                     .output()
+                    .expect("An error occured; Please try again.");
+                Command::new(format!("./{file_name}"))
+                    .output()
                     .expect("An error occured; Please try again.")
             };
+            output.stdout;
         }
         _ => {
             todo!()
