c2396ab39eb4f13e8c4da53f580d9b247a385dc7
TeenCoder159 <no1umang@gmail.com>
2025-01-27 18:47:26 +0530

    Add dependency linking for cpp

--- src/dependency.rs
+++ src/dependency.rs
@@ -7,9 +7,7 @@ pub fn dependency(file_ext: &str, file_name: &String, dep: Option<String>) {
         "py" => {
             let contents = match fs::read_to_string(file_name) {
                 Ok(value) => value,
-                Err(_) => {
-                    "\nprint(\"Hello Lion!\")".to_string()
-                }
+                Err(_) => "\nprint(\"Hello Lion!\")".to_string(),
             };
             util::file_creator(file_name, format!("import {dep}\n{contents}").as_str())
         }
@@ -29,6 +27,15 @@ pub fn dependency(file_ext: &str, file_name: &String, dep: Option<String>) {
             util::file_creator(&String::from("Cargo.toml"), final_content.as_str());
             util::file_creator(file_name, "fn main() {\n    println!(\"Hello Lion!\");\n}");
         }
+        "cpp" => {
+            let contents = match fs::read_to_string(file_name){
+                Ok(value) => value,
+                _ => String::from("#include <iostream>\n\nint main() {\n    std::cout << \"Hello, Lion!\" << std::endl;\n    return 0;\n}")
+            };
+            let final_content = format!("#include \"{dep}/{dep}.h\"\n{contents}");
+            util::file_creator(file_name, final_content.as_str());
+        }
+
         _ => {
             eprintln!("Format not supported for external dependencies");
         }
