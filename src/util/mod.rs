373562b4cab9452c2ccb46423a32f9397ef16c53
TeenCoder159 <no1umang@gmail.com>
2025-01-28 14:23:48 +0530

    Remove testing code

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -1,6 +1,5 @@
 use std::fs;
 
 pub fn file_creator(file_name: &String, file_content: &str) {
-    println!("{file_name} -> file name, {file_content} -> file content");
     fs::write(file_name, file_content).expect("An Unexpected error occured; please try again!");
 }
