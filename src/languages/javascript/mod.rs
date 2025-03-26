ebee4e51375cc84ec93b5f6d4a61a25c2f6ed3e0
TeenCoder159 <no1umang@gmail.com>
2025-03-27 01:51:22 +0800

    Create mod.rs for javascript

---
+++ src/languages/javascript/mod.rs
@@ -0,0 +1,36 @@
+use crate::utils::*;
+use std::process::Command;
+
+pub fn new(file_name: &String) {
+    writer(file_name, "console.log(\"Hello, Lion!\");");
+}
+
+pub fn run(file_name: &String) {
+    if let Err(error) = Command::new("node")
+        .arg(format!("src/{file_name}"))
+        .status()
+    {
+        panic!("error: {error}")
+    }
+}
+
+pub fn proj(proj_name: &String) {
+    if let Err(error) = Command::new("mkdir").arg(proj_name).status() {
+        panic!("error: {error}")
+    }
+    if let Err(error) = Command::new("mkdir")
+        .arg(format!("{proj_name}/src"))
+        .status()
+    {
+        panic!("error: {error}")
+    }
+
+    if let Err(error) = Command::new("npm")
+        .current_dir(proj_name)
+        .arg("init")
+        .arg("-y")
+        .status()
+    {
+        panic!("error: {error}")
+    }
+}
