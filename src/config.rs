ac1bf423b6395aeb45e469749b0a4f43f53aebd5
unsecretised <no1umang@gmail.com>
2025-09-21 22:18:50 +0800

    Add loading config function

--- src/config.rs
+++ src/config.rs
@@ -1,3 +1,5 @@
+use std::path::Path;
+
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Clone)]
@@ -10,3 +12,19 @@ pub struct RustConfig {
     pub new_library_command: Option<String>,
 }
 
+#[derive(Debug, Default, Serialize, Deserialize)]
+pub struct Config {
+    pub rust: Option<RustConfig>,
+}
+
+impl Config {
+    pub fn load(dir: impl AsRef<Path>) -> Self {
+        let config_file = dir.as_ref().join("Lion.toml");
+        if config_file.exists() {
+            let config = std::fs::read_to_string(config_file).unwrap();
+            toml::from_str(&config).unwrap()
+        } else {
+            Config::default()
+        }
+    }
+}
